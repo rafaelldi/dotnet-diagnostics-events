cecfc2ebda425c940b079f180f686ff7d8452718
rival <rival.abdrakhmanov@gmail.com>
2022-06-02 13:08:35 +0300

    Add metrics projects

---
+++ metrics-listener/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics.Tracing;
+using Microsoft.Diagnostics.NETCore.Client;
+using Microsoft.Diagnostics.Tracing;
+
+Console.WriteLine("Metrics listener sample application");
+Console.WriteLine("Specify a target pid");
+var input = Console.ReadLine();
+if (!int.TryParse(input, out var pid))
+{
+    return;
+}
+
+var client = new DiagnosticsClient(pid);
+var arguments = new Dictionary<string, string?>
+{
+    ["RefreshInterval"] = "1",
+    ["Metrics"] = "Example.MyMeter\\my-counter"
+};
+var providers = new List<EventPipeProvider>
+{
+    new("System.Diagnostics.Metrics", EventLevel.Verbose, (long)EventKeywords.All, arguments)
+};
+using var session = client.StartEventPipeSession(providers, false);
+using var source = new EventPipeEventSource(session.EventStream);
+
+Console.CancelKeyPress += (_, eventArgs) =>
+{
+    eventArgs.Cancel = true;
+    session.Stop();
+};
+
+source.Dynamic.All += HandleEvent;
+source.Process();
+
+static void HandleEvent(TraceEvent evt)
+{
+    if (evt.ProviderName != "System.Diagnostics.Metrics" || evt.EventName != "CounterRateValuePublished")
+    {
+        return;
+    }
+
+    var meter = (string)evt.PayloadValue(1);
+    var counter = (string)evt.PayloadValue(3);
+    var value = (string)evt.PayloadValue(6);
+
+    Console.WriteLine($"{meter}/{counter} - {value}");
+}
---
+++ metrics/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics.Metrics;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+
+Console.WriteLine("Metrics sample application");
+Console.WriteLine($"Current process id: {Environment.ProcessId}");
+
+var meter = new Meter("Example.MyMeter");
+var counter = meter.CreateCounter<int>("my-counter");
+
+using var meterProvider = Sdk.CreateMeterProviderBuilder()
+    .AddRuntimeMetrics()
+    .AddMeter("Example.MyMeter")
+    .Build();
+
+using var myMeterListener = new MeterListener();
+myMeterListener.InstrumentPublished = (instrument, listener) =>
+{
+    if (instrument.Meter.Name == "Example.MyMeter")
+    {
+        listener.EnableMeasurementEvents(instrument);
+    }
+};
+myMeterListener.SetMeasurementEventCallback<int>(OnMeasurementWritten);
+myMeterListener.Start();
+
+Task.Run(async () => await StartMetricProducingTask());
+
+Console.ReadKey();
+
+async Task StartMetricProducingTask()
+{
+    while (true)
+    {
+        counter.Add(1);
+        await Task.Delay(200);
+    }
+}
+
+void OnMeasurementWritten(Instrument instrument, int measurement, ReadOnlySpan<KeyValuePair<string, object?>> tags,
+    object? state)
+{
+    Console.WriteLine($"{instrument.Name} - {measurement}");
+}
